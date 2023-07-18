44f24dc7c2ff4ebad0958f170f6541945790fb4b
Mikyy26 <136321245+Mikyy26@users.noreply.github.com>
2023-07-18 14:37:09 +0100

    Add files via upload

---
+++ Chess/src/org/academiadecodigo/gamesweek/SimpleGFX/PiecesImages/PawnImage.java
@@ -0,0 +1,46 @@
+package org.academiadecodigo.gamesweek.SimpleGFX.PiecesImages;
+
+import org.academiadecodigo.gamesweek.Pieces.Piece;
+import org.academiadecodigo.gamesweek.Pieces.Team;
+import org.academiadecodigo.gamesweek.SimpleGFX.PieceImage;
+import org.academiadecodigo.simplegraphics.pictures.Picture;
+
+public class PawnImage extends PieceImage {
+	
+	private final Picture picture;
+	
+	
+	public PawnImage (Piece piece) {
+		
+		super(piece.getPosition());
+		
+		if (piece.getTeam() == Team.WHITE) {
+			this.picture = new Picture(super.getxPosition(), super.getyPosition(), "wP.png");
+		} else {
+			this.picture = new Picture(super.getxPosition(), super.getyPosition(), "bP.png");
+		}
+		
+		this.picture.draw();
+	}
+	
+	
+	@Override
+	public Picture getPicture () {
+		
+		return this.picture;
+	}
+	
+	
+	@Override
+	public void delete () {
+		
+		this.picture.delete();
+	}
+	
+	
+	public void updateImagePosition () {
+		
+		super.updateImagePosition(this.picture);
+	}
+	
+}
